cc4a2c96d40ec80477bcd3fca6109f34ddb45450
Nikhil Verma <lci2020044@iiitl.ac.in>
2023-05-16 15:02:01 +0530

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import webbrowser
 # import sklearn
-from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 from gensim.models import Word2Vec
